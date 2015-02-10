1e4b411230aa887154436cc66aa43676e27513de
pal <pal@zuehlke.com>
2015-02-10 14:23:58 +0100

    Added markup extension for creating ReactiveCommand in XAML

---
+++ ReactiveXAML/MarkupExtensions/ReactiveCommandExtension.cs
@@ -0,0 +1,47 @@
+﻿namespace ReactiveXAML.MarkupExtensions
+{
+    using System;
+    using System.Windows;
+    using System.Windows.Data;
+    using System.Windows.Markup;
+
+    public class ReactiveCommandExtension : MarkupExtension
+    {
+        public ReactiveCommandExtension(string path)
+        {
+            Path = path;
+        }
+
+        public string Path { get; set; }
+
+        public object Source { get; set; }
+
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            var valueTarget = (IProvideValueTarget)serviceProvider.GetService(typeof(IProvideValueTarget));
+
+            var source = Source;
+            var path = Path;
+
+            if (source == null)
+            {
+                var target = valueTarget.TargetObject as FrameworkElement;
+                if (target == null)
+                {
+                    throw new InvalidOperationException("Either Source must be set or this MarkupExtenstion must be used on a FrameworkElement.");
+                }
+
+                source = target;
+
+                path = "DataContext." + path;
+            }
+
+            var binding = new Binding(path) { Source = source };
+
+            var command = new ReactiveCommand();
+            command.SetBinding(ReactiveCommand.CalledObserverProperty, binding);
+
+            return command;
+        }
+    }
+}
